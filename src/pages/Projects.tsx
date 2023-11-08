0a731b79e8e7f95cc769827b6be68a231de88987
matheushenriquecsb <matheus.mhg2@gmail.com>
2023-11-08 09:12:19 -0300

    feat: create projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,18 @@
+import ProjectItem from "../components/ProjectItem/ProjectItem";
+import "../components/ProjectItem/projectItem.css";
+import { projectLists } from "../helpers/ProjectList";
+
 function Projects() {
-  return <div>projects</div>;
+  return (
+    <div className="projects">
+      <h1>My Personal Projects</h1>
+      <div className="projectList">
+        {projectLists.map((projects) => {
+          return <ProjectItem name={projects.name} image={projects.image} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
