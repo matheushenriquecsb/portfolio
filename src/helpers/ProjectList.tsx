a4a626960d5bef3de8ae75c13108d27a1117fcad
Matheus <matheus.mhg2@gmail.com>
2024-02-26 18:04:36 -0300

    feat: added burguer order project

--- src/helpers/ProjectList.tsx
+++ src/helpers/ProjectList.tsx
@@ -1,23 +1,8 @@
 import proj1 from "../assets/booking_project.png";
-import proj2 from "../assets/oauth_project.png";
-import proj3 from "../assets/qrcode_project.png";
 import proj4 from "../assets/github_project.png";
+import proj3 from "../assets/burguer_order.png";
 
 export const projectLists = [
-  {
-    name: "Login Auth",
-    image: proj2,
-    skills: "React, Javascript, CSS",
-    github: "https://github.com/matheushenriquecsb/login-oauth-frontend",
-    link: "https://login-oauth.netlify.app",
-  },
-  {
-    name: "QrCode Generator",
-    image: proj3,
-    skills: "React, Javascript, CSS",
-    github: "https://github.com/matheushenriquecsb/generator-codeQR",
-    link: "https://generator-qrcode-react.netlify.app",
-  },
   {
     name: "Github Finder",
     image: proj4,
@@ -26,9 +11,17 @@ export const projectLists = [
     link: "https://githubfinder-api.netlify.app/",
   },
   {
+    name: "Burguer Order",
+    image: proj3,
+    skills: "VueJS, CSS",
+    github: "https://github.com/matheushenriquecsb/order-menu-vuejs",
+    link: "https://burguer-order-menu.netlify.app",
+  },
+  {
     name: "Booking Website",
     image: proj1,
     skills: "React, Javascript, CSS",
     github: "https://github.com/matheushenriquecsb/booking-website-ui",
+    link: "https://booking-rent.netlify.app",
   },
 ];
